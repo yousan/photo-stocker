557b69fed1ef80231d10808d0a64067a472dc28a
Yousan_O <yousan@l2tp.org>
2017-01-02 00:47:54 +0900

    Automatically commit

--- src/photo-stocker.php
+++ src/photo-stocker.php
@@ -7,13 +7,26 @@
  */
 
 Class PhotoStocker {
-	private static $sourcedir = __DIR__.'../storage/';
-	private $destdir = __DIR__.'../storage/';
+	private static $sourcedir = __DIR__.'/../photos/';
+	private static $destbasedir = __DIR__ . '/../storage/';
+	private static $dirmode = 0755;
+
 	public static function stock() {
-		$file = self::sourcedir . 'P1017630.JPG';
-		$exifdata = exif_read_data("画像パス", 0, true);
+		$sourcefile = self::$sourcedir . 'P1017630.JPG';
+		$exifdata = exif_read_data($sourcefile, 0, true);
+		var_dump($sourcefile);
 		$date = isset($exifdata["EXIF"]['DateTimeOriginal']) ? $exifdata["EXIF"]['DateTimeOriginal'] : "";
-		echo $date;
+		$date = DateTimeImmutable::createFromFormat('Y:m:d H:i:s', $date);
+		var_dump($date->format('Y-m-d H:i:s'));
+		$year = $date->format('Y');
+		$month = $date->format('m');
+		$destdir = self::$destbasedir . '/' . $year . '/' . $month . '/';
+		$destfile = $destdir . basename($sourcefile);
+		if ( !file_exists($destdir) ) { // ディレクトリがなかったら作成
+			echo 'Creating Directory: '. $destdir;
+			mkdir($destdir, self::$dirmode, true);
+		}
+		rename($sourcefile, $destfile);
 	}
 }
 
