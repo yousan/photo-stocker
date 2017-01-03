495662c70e40e6a27cd70f5aa24465fa0a0f7787
Yousan_O <yousan@l2tp.org>
2017-01-03 23:30:43 +0900

    add pattern

--- src/photo-stocker.php
+++ src/photo-stocker.php
@@ -11,11 +11,32 @@ Class PhotoStocker {
 	private static $destbasedir = __DIR__ . '/../storage/';
 	private static $dirmode = 0755;
 
+	/**
+	 * 移動対象とするファイル名のパターン。
+	 * デフォルトは.jpeg, .jpgで大文字小文字を判定しない。
+	 *
+	 * @var string
+	 */
+	private static $filename_pattern = '/.(jpg|jpeg)$/i';
+
 	public static function stock() {
-		$sourcefile = self::$sourcedir . 'P1017630.JPG';
-		self::move_file($sourcefile);
+		foreach ( scandir(self::$sourcedir) as $filename ) {
+			if ( in_array($filename, array(".","..")) ) {
+				continue; // .と..
+			}
+			if ( preg_match(self::$filename_pattern, $filename) ) {
+				$sourcefile = self::$sourcedir . $filename;
+				//var_dump($sourcefile);
+				self::move_file($sourcefile);
+			}
+		}
 	}
 
+	/**
+	 * ファイルを移動する
+	 *
+	 * @param $sourcefile
+	 */
 	private static function move_file($sourcefile) {
 		$exifdata = exif_read_data($sourcefile, 0, true);
 		if ( !isset($exifdata["EXIF"]['DateTimeOriginal']) ) {
@@ -23,14 +44,18 @@ Class PhotoStocker {
 		}
 		$date = DateTimeImmutable::createFromFormat('Y:m:d H:i:s', $exifdata["EXIF"]['DateTimeOriginal']);
 		$year = $date->format('Y');
-		$month = $date->format('m');
+		$month = $date->format('Ym');
 		$destdir = self::$destbasedir . '/' . $year . '/' . $month . '/';
 		$destfile = $destdir . basename($sourcefile);
 		if ( !file_exists($destdir) ) { // ディレクトリがなかったら作成
-			echo 'Creating Directory: '. $destdir;
+			echo 'Creating Directory: '. $destdir . PHP_EOL;
 			mkdir($destdir, self::$dirmode, true);
 		}
-		rename($sourcefile, $destfile);
+		if ( file_exists($destfile) ) {
+			echo 'File already exists: '. $destfile . PHP_EOL;
+		} else {
+			rename($sourcefile, $destfile);
+		}
 	}
 }
 
